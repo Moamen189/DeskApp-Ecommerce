c932d56bbd157310ecb5f9d0a6a1685dfac8e3ff
Mo'men Ashraf Mohamed <moamen.ashraf1892001@gmail.com>
2022-08-12 05:32:48 +0200

    OrderController

--- Ecommerce/Controllers/OrderController.cs
+++ Ecommerce/Controllers/OrderController.cs
@@ -11,18 +11,20 @@ namespace Ecommerce.Controllers
     public class OrderController : Controller
     {
         private readonly IGenericRepository<Order> _repository;
+        private readonly IOrderProductRepo _productRepo;
         private readonly IMapper _mapper;
 
-        public OrderController(IGenericRepository<Order> repository , IMapper mapper)
+        public OrderController(IGenericRepository<Order> repository ,IOrderProductRepo productRepo ,IMapper mapper)
         {
             _repository = repository;
+            _productRepo = productRepo;
             _mapper = mapper;
         }
 
         public async Task<IActionResult> Index()
         {
             var data = await _repository.GetAll();
-            var mappedOrder = _mapper.Map<IEnumerable <OrderVM>>(data);
+            var mappedOrder = _mapper.Map<IEnumerable <OrderVM> >(data);
             return View(mappedOrder);
         }
 
@@ -42,21 +44,64 @@ namespace Ecommerce.Controllers
         {
             return View();
         }
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Create(OrderVM Order)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var mappedData = await _repository.Create(_mapper.Map<Order>(Order));
-        //        foreach (var item in Order.ProductId)
-        //        {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(OrderVM Order)
+        {
+            if (ModelState.IsValid)
+            {
+                var mappedData = await _repository.Create(_mapper.Map<Order>(Order));
+                foreach (var item in Order.ProductId)
+                {
+                    _productRepo.Create(mappedData, item);
+
+                }
+                return RedirectToAction("Index");
+
+                 
+
+            }
+            return View(Order);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var data = await _repository.GetById(id);
+            await _repository.Delete(data);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var data = await _repository.GetById(id);
+
+            if(data == null)
+                return BadRequest();
+            return View(_mapper.Map<OrderVM>(data));
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(OrderVM order)
+        {
+            if (ModelState.IsValid)
+            {
+                 await _repository.Update(_mapper.Map<Order>(order));
+               
+                return RedirectToAction("Index");
 
 
-        //        }
+
+            }
+            return View(order);
+
+        }
 
 
-        //    }
-        //}
     }
 }
